0daa98f70caf7a0f2cfebb04cb25023e7d5e7d1a
Julio Gomez <jgod93@Julios-MacBook-Pro-20.local>
2021-02-24 22:25:01 -0500

    Created Classes Router

---
+++ api/classes/classes-model.js
@@ -0,0 +1,11 @@
+// scheme-model
+const db = require('../../data/db-config')
+
+module.exports = {
+    find
+};
+
+function find() {
+    return db('classes');
+}
+
---
+++ api/classes/classes-router.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const Classes = require('./classes-model')
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    Classes.find()
+    .then(classes => {
+        res.json(classes);
+    })
+    .catch(err => {
+        res.status(500).json({ message: 'Failed to get classes'})
+    })
+});
+
+module.exports = router;
