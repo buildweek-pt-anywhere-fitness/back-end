3a4c728e9e2d794d96b6a0d7b71e8dc6857c9cbd
Julio Gomez <jgod93@Julios-MacBook-Pro-20.local>
2021-02-26 23:08:45 -0500

    Updated Router and Model

--- api/classes/classes-model.js
+++ api/classes/classes-model.js
@@ -2,10 +2,43 @@
 const db = require('../../data/db-config')
 
 module.exports = {
-    find
+    find,
+    add,
+    findById,
+    update,
+    remove,
+
 };
 
 function find() {
     return db('classes');
 }
 
+function add(classes) {
+    return db('classes').insert(classes)
+    // .then(object => {
+    //     return findById(object[0]);
+    // })
+}
+
+function findById(id) {
+    return db('classes')
+    .where({id})
+    .first();
+}
+
+function update(changes, id) {
+    return db('classes')
+    .where({ id })
+    .update(changes)
+    .then(count => {
+        return findById(id)
+    });
+}
+
+
+function remove(id) {
+    return db('classes')
+    .where({id})
+    .del();
+}
